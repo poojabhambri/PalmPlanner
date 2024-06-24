92565965fa0d286afbdd71967040b10555b3bec2
Shraddha-Mehta <149219530+Shraddha-Mehta@users.noreply.github.com>
2024-06-24 00:43:36 -0600

    login and register page buttons work on creating and logging in now

--- app/src/main/java/com/example/palmplanner/LoginActivity.java
+++ app/src/main/java/com/example/palmplanner/LoginActivity.java
@@ -16,6 +16,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class LoginActivity extends AppCompatActivity {
     private EditText usernameEditText, passwordEditText;
     private Button loginButton, createAccButton;
+
+    private FirebaseFirestore db;
     private FirebaseAuth mAuth;
 
     @Override
@@ -23,5 +25,43 @@ public class LoginActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Firebase authentication
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        //Initialize the views
+        usernameEditText= findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        createAccButton= findViewById(R.id.createAccButton);
+        loginButton = findViewById(R.id.loginButton);
+
+        //click listeners for buttons
+        createAccButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser(){
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if(username.isEmpty()|| password.isEmpty()){
+            Toast.makeText(LoginActivity.this, "Fill in both username and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
     }
 }
